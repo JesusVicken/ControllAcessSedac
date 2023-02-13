97d033c50c4d020a4630b61a1229c72b11e7575c
JesusVicken <jesus.viccken@gmail.com>
2023-02-13 15:53:08 -0300

    first commit Sedac

---
+++ js/login.js
@@ -0,0 +1,12 @@
+function logar(){
+    var email = document.getElementById("email");
+    var senha = document.getElementById("senha");
+
+    if (email.value === "admin@admin" && senha.value === "admin"){
+        localStorage.setItem("acesso", true);
+
+        window.location.href = "request.html";
+    }else{
+        window.alert("Usuário ou senha inválida");
+    }  
+}
